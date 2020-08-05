8637aef7d94237c3da31cb41f0a3c0ca5738f14a
CaptainBateau <matteohx@gmail.com>
2020-08-05 09:47:25 +0200

    Push point light and rendering pipeline

---
+++ Scripts/CharacterMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    Rigidbody2D _rb;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+            
+    }
+
+
+
+}
