8f2cad69c7bff325b4c661f5658ab1e291bb622f
CaptainBateau <matteohx@gmail.com>
2020-08-13 15:01:19 +0200

    InputAssigner/ Interaction Door/Stairs

---
+++ Scripts/StairsInteraction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairsInteraction : MonoBehaviour
+{
+    public Transform _exitStairs;
+    bool _triggered = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(GameState._interactButton))
+        {
+            if (_triggered)
+            {
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                player.transform.position = player.transform.position + (_exitStairs.position - transform.position);
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            _triggered = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _triggered = false;
+        }
+    }
+}
