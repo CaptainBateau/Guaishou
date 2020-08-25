5502b9a3aeff3449922bfe2c509c1bb199f57346
Neven Axel <neven.axel@gmail.com>
2020-08-25 09:38:06 +0200

    tentacle script wip :octopus:

---
+++ Scripts/Monsters/TentacleMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleMovement : MonoBehaviour
+{
+    BoxCollider movementZone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        movementZone.bounds;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
