8d750548a34cbf511ec141e841dab61e184db1c1
NevenAxel <neven.axel@gmail.com>
2020-08-26 11:56:23 +0200

    fixed neckMovement + now shifting head toward its current direction (or player)

--- Scripts/Monsters/AttackingNeck.cs
+++ Scripts/Monsters/AttackingNeck.cs
@@ -15,14 +15,29 @@ public class AttackingNeck : MonoBehaviour
 
 
     [SerializeField] Transform originalNeckPos = null;
+    Vector2 _dir = Vector2.left;
+    bool inMovement = false;
 
     void Start()
     {
+        detectionEvent.OnShiftDirection += OnShiftDirection;
         detectionEvent.OnPlayerDetected += OnPlayerDetectedHandler;
         detectionEvent.OnPlayerNotDetectedAnymore += OnPlayerNotDetectedAnymoreHandler;
     }
 
-    
+    private void OnShiftDirection(object sender, MonsterDetectionEvent.ShiftDirectionEventArgs e)
+    {
+        _dir = e.newDir;
+        if(_dir == Vector2.left)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if(_dir == Vector2.right)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+    }
+
     private void OnPlayerDetectedHandler(object sender, MonsterDetectionEvent.PlayerDetectedEventArgs e)
     {
         StartCoroutine(NeckDown());
@@ -36,12 +51,24 @@ public class AttackingNeck : MonoBehaviour
 
     IEnumerator NeckDown()
     {
-        StartCoroutine(Movements.Move(neckBase, targetPos, neckDownCurve, neckDownDuration));
+        while (inMovement)
+        {
+            yield return null;
+        }
+        inMovement = true;
+        StartCoroutine(Movements.Move(neckBase, targetPos, neckDownCurve, neckDownDuration, 0, 0, true));
         yield return new WaitForSeconds(neckDownDuration);
+        inMovement = false;
     }
     IEnumerator NeckRecover()
     {
-        StartCoroutine(Movements.Move(neckBase, originalNeckPos, neckRecoverCurve, neckRecoverDuration));
+        while (inMovement)
+        {
+            yield return null;
+        }
+        inMovement = true;
+        StartCoroutine(Movements.Move(neckBase, originalNeckPos, neckRecoverCurve, neckRecoverDuration, 0, 0, true));
         yield return new WaitForSeconds(neckRecoverDuration);
+        inMovement = false;
     }
 }
