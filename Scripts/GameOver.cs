905d1c3ee144d131440f1bc0968e0128ceb3c4c6
Neven Axel <neven.axel@gmail.com>
2020-08-27 15:51:41 +0200

    gameover

---
+++ Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animation))]
+public class GameOver : MonoBehaviour
+{
+    PlayerEvent playerEvent;
+    Animation anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animation>();
+        playerEvent = FindObjectOfType<PlayerEvent>();
+        playerEvent.OnPlayerGotHit += OnPlayerHit;
+    }
+
+    private void OnPlayerHit(object sender, PlayerEvent.PlayerGotHitEventArgs e)
+    {
+        anim.Play();
+        Invoke("EndGame", anim.clip.averageDuration);
+    }
+
+    void EndGame()
+    {
+        playerEvent.GameOver(new PlayerEvent.GameOverEventArgs { });
+    }
+}
