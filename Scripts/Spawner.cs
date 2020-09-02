c2e117e36b6a610563c5a5bbea8ef2cb7ead8f38
Neven Axel <neven.axel@gmail.com>
2020-09-02 12:54:22 +0200

    mobSpawner working

---
+++ Scripts/Spawner.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] GameObject thingToSpawn;
+    [SerializeField] bool autoSpawn;
+    [SerializeField] float delay = 6;
+    [SerializeField] float randomRangeDelay = 8;
+    bool isSpawning = false;
+    float timer = 0;
+    float actualDelay;
+
+    public void Spawn()
+    {
+        Instantiate(thingToSpawn);
+    }
+    public void StartSpawning()
+    {
+        isSpawning = true;
+    }
+
+    public void StopSpawning()
+    {
+        isSpawning = false;
+    }
+
+    private void Start()
+    {
+        actualDelay = delay;
+        if (autoSpawn)
+            isSpawning = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > actualDelay && isSpawning)
+        {
+            actualDelay = Random.Range(delay - randomRangeDelay / (float)2, delay + randomRangeDelay / (float)2);
+            Spawn();
+            timer = 0;
+        }
+    }
+}
