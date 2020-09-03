d92fd56bbd90fd304211f3115925fa3ce95aa7f5
CaptainBateau <matteohx@gmail.com>
2020-09-03 17:14:44 +0200

    PlayerInteraction

---
+++ Scripts/PlayerInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    bool _activable = false;
+    public UnityEvent _activation;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            _activable = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            _activable = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_activable)
+            {
+                _activation.Invoke();
+            }
+        }
+    }
+}
