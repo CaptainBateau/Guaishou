acbcf0a82632fb20f4f8fbe9b03ec2556e1f3760
Neven Axel <neven.axel@gmail.com>
2020-09-30 22:44:26 +0200

    ProjectileInteraction added
    J'étais pas sur de si tu voulais un système de point de vie mais c'est là au cas où :smile:

---
+++ Scripts/ProjectileInteraction.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ProjectileInteraction : MonoBehaviour
+{
+    bool _activable = true;
+    public int _healthPoint = 1;
+    public bool _oneTimeEffect = false;
+    public bool _destroyProjectileOnCollision = true;
+    public UnityEvent _activation;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        CheckActivation(collision.gameObject);
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        CheckActivation(collision.collider.gameObject);
+    }
+
+    private void CheckActivation(GameObject projectile)
+    {
+        if (projectile.tag == "Projectile")
+        {
+            if (_activable)
+            {
+                _healthPoint--;
+                if (_healthPoint <= 0)
+                {
+                    _activation.Invoke();
+                    if (_oneTimeEffect)
+                        _activable = false;
+                }
+                if (_destroyProjectileOnCollision)
+                    Destroy(projectile);
+            }
+        }       
+    }
+}
+
+
