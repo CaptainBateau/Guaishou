145a08f50f9517def0eeb1d2a1fe7eedbc44ac38
EnnioMan <apopheniamailbox@gmail.com>
2020-10-02 15:26:41 +0200

    build gameforce

---
+++ Scripts/hideCursor2.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hideCursor2 : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //Set Cursor to not be visible
+        Cursor.visible = false;
+    }
+}
